29a4f20427b4e639f8bda8c6fa30aa6617652de6
ashel028 <132619761+ashel028@users.noreply.github.com>
2023-10-19 20:30:07 -0300

    Add files via upload

--- clase 5-backend/index.php
+++ clase 5-backend/index.php
@@ -1,64 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>axel</title>
-    <link rel="stylesheet" href="./css/estilo.css">
-    <style>
+<?php
+// Ejercicio a
+$primeros_pares = [];
+for ($i = 2; $i <= 20; $i += 2)
+{
+    $primeros_pares[] = $i;
+}
+echo "a) Los primeros 10 números pares son: <br>";
+foreach ($primeros_pares as $par)
+{
+    echo $par . "<br>";
+}
 
-    </style>
-        body
-    </style>
-</head>
-<body>
-    <div class="container">
-        <h2>Login</h2>
-        <form action="#" method="post">
-            <label for="username">Usuario:</label>
-            <input type="text" id="username" name="username" required>
+// Ejercicio b
+$datos = ["Pedro", "Ana", 34, 1];
+echo "<br>b) Esquema del array con print_r(): <br>";
+print_r($datos);
 
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" required>
+// Ejercicio c
+$informacion =
+[
+    "Nombre" => "Pedro";
+    "Apellido" => "Torres";
+    "Dirección" => "Av. Mayo 3703";
+    "Teléfono" => "1122334455";
+];
+echo "<br>c) Array asociativo de información personal: <br>";
+print_r($informacion);
 
-            <button type="submit">Iniciar Sesión</button>
-        </form>
-    </div>
-    <div class="php">
+// Ejercicio d
+$ciudades =
+["Madrid" "Barcelona" "Londres" "New York" "Los Ángeles" "Chicago"];
+echo "<br>d) Ciudades: <br>";
+foreach ($ciudades as $index => $ciudad)
+{
+    echo "La ciudad con el índice $index tiene el nombre $ciudad <br>";
+}
 
-      <?php
-  if (isset($_POST['nombre']) && isset($_POST['nota'])) {
-      $nombre = $_POST['nombre'];
-      $nota = $_POST['nota'];
-
-      if (is_numeric($nota)) {
-          if ($nota >= 0 && $nota <= 10) {
-              if ($nota >= 0 && $nota <= 2) {
-                  $calificacion = "Muy deficiente";
-              } elseif ($nota >= 3 && $nota <= 5) {
-                  $calificacion = "Insuficiente";
-              } elseif ($nota >= 6 && $nota <= 7) {
-                  $calificacion = "Bien";
-              } elseif ($nota >= 8 && $nota <= 9) {
-                  $calificacion = "Notable";
-              } elseif ($nota == 10) {
-                  $calificacion = "Sobresaliente";
-              }
-              echo "Nombre del alumno: $nombre<br>";
-              echo "Calificación: $calificacion";
-          } else {
-              echo "La nota debe estar entre 0 y 10.";
-          }
-      } else {
-          echo "La nota no es un número válido.";
-      }
-  } else {
-      echo "Faltan datos. Por favor, ingresa el nombre y la nota del alumno.";
-  }
-  ?>
-
-
-    </div>
-</body>
-</html>
-
+// Ejercicio e
+$ciudades_asociativas =
+ [
+    "MD" => "Madrid";
+    "BCL" => "Barcelona";
+    "LD" => "Londres";
+    "NY" => "New York";
+    "LA" => "Los Ángeles";
+    "CCG" => "Chicago";
+];
+echo "<br>e) Ciudades asociativas: <br>";
+foreach ($ciudades_asociativas as $index => $ciudad) {
+    echo "El índice de $ciudad es $index <br>";
+}
+?>

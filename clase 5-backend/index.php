b867e19eb75011e1f862d4ef188dd916a9a1b716
ashel028 <132619761+ashel028@users.noreply.github.com>
2023-10-24 20:54:04 +0200

    Update index.php

--- clase 5-backend/index.php
+++ clase 5-backend/index.php
@@ -1,121 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-   <link rel="stylesheet" href="styles.css">
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f2f2f2;
-            font-family: Arial, sans-serif;
-        }
-
-        .login-container {
-            background-color: #fff;
-            padding: 100px;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-
-        h2 {
-            text-align: center;
-        }
-
-        .input-group {
-            margin-bottom: 20px;
-        }
-
-        .input-group label {
-            display: block;
-            margin-bottom: 5px;
-        }
-
-        .input-group input {
-            width: 100%;
-            padding: 10px;
-            border-radius: 5px;
-            border: 1px solid #ddd;
-            box-sizing: border-box;
-        }
-
-        button {
-            width: 100%;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            background-color: #4caf50;
-            color: #fff;
-            cursor: pointer;
-        }
-    </style>
+    <title>Clase 6 - Tarea PD - Programación Back End</title>
 </head>
+
 <body>
-    <div class="login-container">
-        <h2>Login</h2>
-        <form action="#" method="POST">
-            <div class="input-group">
-                <label for="username">Username</label>
-                <input type="text" id="username" name="username" required>
-            </div>
-            <div class="input-group">
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
-            </div>
-            <button type="submit">Login</button>
-        </form>
-    </div>
+    <h2>Tarea 1: Factorial de 8</h2>
+    <?php
+    // Tarea 1: Factorial de 8
+    $num = 8;
+    $factorial = 1;
+    for ($i = 1; $i <= $num; $i++) {
+        $factorial *= $i;
+    }
+    echo "<p>El factorial de 8 es: " . $factorial . "</p>";
 
+    // Tarea 2 y 3: Calificación del alumno
+    if (isset($_POST['submit'])) {
+        $alumno = $_POST['alumno'];
+        $nota = $_POST['nota'];
+        $mensaje = "";
 
-<?php
-// 1) Mostrar los números del 1 al 9.
-echo "1) Mostrar los números del 1 al 9: ";
-for ($i = 1; $i <= 9; $i++) {
-    echo $i . " ";
-}
-echo "\n";
+        if (empty($alumno)) {
+            $mensaje = "El nombre del alumno no ha sido ingresado.";
+        } elseif (empty($nota)) {
+            $mensaje = "La nota no ha sido ingresada.";
+        } elseif (!is_numeric($nota)) {
+            $mensaje = "La nota no es un número.";
+        } elseif ($nota < 0 || $nota > 10) {
+            $mensaje = "La nota no está en el rango válido (0-10).";
+        } else {
+            if ($nota >= 0 && $nota <= 2) {
+                $mensaje = "Muy deficiente";
+            } elseif ($nota >= 3 && $nota <= 5) {
+                $mensaje = "Insuficiente";
+            } elseif ($nota >= 6 && $nota <= 7) {
+                $mensaje = "Bien";
+            } elseif ($nota >= 8 && $nota <= 9) {
+                $mensaje = "Notable";
+            } elseif ($nota === 10) {
+                $mensaje = "Sobresaliente";
+            }
+            echo "<h2>Calificación de $alumno</h2>";
+            echo "<p>La nota ingresada es: $nota - $mensaje</p>";
+        }
+        if (!empty($mensaje)) {
+            echo "<p style='color: red;'>$mensaje</p>";
+        }
+    }
+    ?>
 
-// 2) Mostrar los números del 9 al 1.
-echo "2) Mostrar los números del 9 al 1: ";
-for ($i = 9; $i >= 1; $i--) {
-    echo $i . " ";
-}
-echo "\n";
-
-// 3) Mostrar los números pares del 1 al 20.
-echo "3) Mostrar los números pares del 1 al 20: ";
-for ($i = 2; $i <= 20; $i += 2) {
-    echo $i . " ";
-}
-echo "\n";
-
-// 4) Mostrar los números impares del 1 al 20.
-echo "4) Mostrar los números impares del 1 al 20: ";
-for ($i = 1; $i <= 20; $i += 2) {
-    echo $i . " ";
-}
-echo "\n";
-
-// 5) Mostrar la suma de los números del 1 al 20.
-$sum = 0;
-for ($i = 1; $i <= 20; $i++) {
-    $sum += $i;
-}
-echo "5) La suma de los números del 1 al 20 es: " . $sum . "\n";
-
-// 6) Mostrar la suma de los números pares del 1 al 20.
-$evenSum = 0;
-for ($i = 2; $i <= 20; $i += 2) {
-    $evenSum += $i;
-}
-echo "6) La suma de los números pares del 1 al 20 es: " . $evenSum . "\n";
-?>
-
+    <h2>Tarea 2: Formulario de calificación</h2>
+    <form action="" method="post">
+        <label for="alumno">Nombre del alumno:</label><br>
+        <input type="text" id="alumno" name="alumno"><br><br>
+        <label for="nota">Nota:</label><br>
+        <input type="text" id="nota" name="nota"><br><br>
+        <input type="submit" name="submit" value="Enviar">
+    </form>
 </body>
+
 </html>
+

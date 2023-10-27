949d1da235bcd25bb5e98e798322c98c569ad088
ashel028 <132619761+ashel028@users.noreply.github.com>
2023-10-27 12:13:44 -0300

    Update index.php

--- clase 5-backend/index.php
+++ clase 5-backend/index.php
@@ -3,77 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alta y Edición de Producto</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Aquí debes vincular tu archivo de CSS -->
+    <link rel="stylesheet" href="styles.css">
+    <title>Tabla de Gestión de Productos</title>
 </head>
 <body>
-    <div class="form-container">
-        <h2>Alta de Producto</h2>
-        <form action="procesar.php" method="post">
-            <label for="product-name">Nombre del Producto:</label>
-            <input type="text" id="product-name" name="product-name" required>
-            <label for="product-description">Descripción del Producto:</label>
-            <textarea id="product-description" name="product-description" required></textarea>
-            <label for="product-price">Precio del Producto:</label>
-            <input type="number" id="product-price" name="product-price" required>
-            <button type="submit">Guardar Producto</button>
-        </form>
+  <table class="table table-dark table-sm">
+
+    <div class="form-row">
+<div class="form-group col-md-6">
+    <div class="container">
+        <h2>Gestión de Productos</h2>
+        <table>
+            <tr>
+                <th>Nombre del Producto</th>
+                <th>Precio</th>
+                <th>Stock</th>
+            </tr>
+            </div>
+            </div>
+
+            <tr>
+                <td>Producto 1</td>
+                <td>$10</td>
+                <td>50</td>
+            </tr>
+
+
+            <tr>
+                <td>Producto 2</td>
+                <td>$15</td>
+                <td>30</td>
+            </tr>
+
+            <tr>
+                <td>Producto 3</td>
+                <td>$20</td>
+                <td>20</td>
+            </tr>
+
+            <tr>
+                <td>Producto 4</td>
+                <td>$25</td>
+                <td>10</td>
+            </tr>
+
+            <tr>
+                <td>Producto 5</td>
+                <td>$30</td>
+                <td>05</td>
+            </tr>
+
+        </table>
     </div>
-
-
-  <div class="php-conteiner">
-  <?php 
-
-   
-    $numbers = [];
-    for ($i = 1; count($numbers) < 10; $i++) {
-        if ($i % 2 === 0) {
-            $numbers[] = $i;
-        }
-    }
-    foreach ($numbers as $number) {
-        echo $number . "<br>";
-    }
-
-   
-    $array = ['Pedro', 'Ana', 34, 1];
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
-
-    
-    $person = ['Nombre' => 'Pedro',
-        'Apellido' => 'Torres',
-        'Dirección' => 'Av. Mayo 3703',
-        'Teléfono' => '1122334455'
-    ]; echo "<pre>";
-    print_r($person);
-    echo "</pre>";
-
-    
-    $cities = ['Madrid', 'Barcelona', 'Londres', 'New York', 'Los Ángeles', 'Chicago'];
-    foreach ($cities as $index => $city) {
-        echo "La ciudad con el índice $index tiene el nombre $city <br>";
-    }
-
-    $citiesAssociative = [
-        'MD' => 'Madrid',
-        'BCL' => 'Barcelona',
-        'LD' => 'Londres',
-        'NY' => 'New York',
-        'LA' => 'Los Ángeles',
-        'CCG' => 'Chicago'
-    ];
-
-    foreach ($citiesAssociative as $index =>       
-  $city) {
-        echo "El índice de $city es $index <br>";
-    }
-
-    ?>
-  </div>
-
-  
+    </table>
 </body>
 </html>
-
